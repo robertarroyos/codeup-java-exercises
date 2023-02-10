097583a984a614e33dbfceebb92596ae3afbd92d
Robert Arroyos <robertarroyosjr@gmail.com>
2023-02-10 10:40:16 -0600

    Do-While Loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,14 +3,13 @@ import java.sql.SQLOutput;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        Loop Basics
+//        Loop Basics - Do-While Loop
 
-        int i = 5;
-
-        while ( i < 16) {
+        int i = 0;
+        do {
             System.out.println(i);
             i++;
-        }
+        } while ( i < 101 );
 
     }
 }
