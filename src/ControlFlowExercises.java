81c47bde5a5810dd488c16f5a0e2a32d1605e693
Robert Arroyos <robertarroyosjr@gmail.com>
2023-02-10 13:25:41 -0600

    Multiplication Table Exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,24 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
 //        Loop Basics - Do-While Loop
 
-        for (int i = 1; i < 101; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Fuzz");
-            } else System.out.println(i);
+        Scanner input = new Scanner(System.in);
+        System.out.print("What number would you like to go up to? ");
+        int userNum = input.nextInt();
+
+        System.out.printf(
+                "%nHere is your table!%n%nnumber | squared | cubed%n------ | ------- | ------"
+        );
+
+        for ( int i = 1; i < userNum + 1; i++ ) {
+            int iSq = i * i;
+            int iCb = i * i * i;
+            System.out.printf(
+                    "%n%-7d| %-8d| %d", i, iSq, iCb
+            );
         }
 
     }
